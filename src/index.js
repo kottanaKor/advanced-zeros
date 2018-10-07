94d2c3b79a5b97a145b3ac052fd5642bf959f4ed
Tatsiana Korshunava <tachichoco@yahoo.com>
2018-10-07 21:05:22 +0300

    79 pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let primesArr = [];
+
+  for (let i = 2; i <= Math.sqrt(base); i++) {
+    if (base % i === 0) {
+      while (base % i === 0) {
+        base = base / i;
+        primesArr.push(i);
+      }
+    }
+  }
+
+  if (base != 1) {
+    primesArr.push(base);
+  }
+
+  let maxValue = Math.max.apply(null, primesArr);
+
+  let zeros = 0;
+  let sum = number;
+  let divider = maxValue;
+
+  while (sum >= maxValue) {
+    sum = Math.floor(number / divider);
+    divider *= maxValue;
+    zeros += sum;
+  }
+
+  return zeros;
+  
 }
